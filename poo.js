df2854237550de685c6991e01b0dbcced8ff7a63
Aaron <aaronloyo2010@gmail.com>
2023-05-11 12:15:53 -0300

    Last Commit

--- poo.js
+++ poo.js
@@ -12,12 +12,17 @@ function CreateStudent(name,password) {
 function DarValorInput(id) {
     return document.getElementById(id).value;
 }
-let cursitos
+let curso1 = document.getElementById("curso1")
+let curso2 = document.getElementById("curso2")
+let curso3 = document.getElementById("curso3")
+let curso4 = document.getElementById("curso4")
+let cursitos = [document.getElementById("curso1"),document.getElementById("curso2"),document.getElementById("curso3"),document.getElementById("curso4"),]
 let seccion = document.querySelector(".register-zone")
 let username = undefined
 let password = undefined
 let sendbtn = document.getElementById("send")
 let user = undefined;
+let approvedCoursesSeccion = document.getElementById("aprovarcursoseccion")
 function comenzarApp() {
     username = DarValorInput("Nombre")
     password = DarValorInput("Contraseña")
@@ -34,4 +39,24 @@ function comenzarApp() {
         document.getElementById("cursos").classList.remove("invisible")
     }
 }
+function rederizarcurso1() {
+    user.newCourse("Curso de hackeo en cafeteria")
+    document.getElementById("curso1terminado").innerHTML = "Terminado"
+}
+function rederizarcurso2() {
+    user.newCourse("Curso de hackeo de robots")
+    document.getElementById("curso2terminado").innerHTML = "Terminado"
+}
+function rederizarcurso3() {
+    user.newCourse("Curso de virus avanzado")
+    document.getElementById("curso3terminado").innerHTML = "Terminado"
+}
+function rederizarcurso4() {
+    user.newCourse("curso de virus basico")
+    document.getElementById("curso4terminado").innerHTML = "Terminado"
+}
 sendbtn.addEventListener("click", comenzarApp)
+curso1.addEventListener("click",rederizarcurso1)
+curso2.addEventListener("click",rederizarcurso2)
+curso3.addEventListener("click",rederizarcurso3)
+curso4.addEventListener("click",rederizarcurso4)
