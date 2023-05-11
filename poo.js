10febb5b695df0f7e24f077d80e12acca07c2409
Aaron <aaronloyo2010@gmail.com>
2023-05-11 11:18:57 -0300

    Primer Commit Con el Proyecto ya avanzado

---
+++ poo.js
@@ -0,0 +1,34 @@
+function CreateStudent(name,password) {
+    return {
+        name: name,
+        password: password,
+        email: name + "@gmail.com",
+        approvedCourses: []
+    }
+}
+function DarValorInput(id) {
+    return document.getElementById(id).value;
+}
+let cursitos
+let seccion = document.querySelector(".register-zone")
+let username = undefined
+let password = undefined
+let sendbtn = document.getElementById("send")
+let user = undefined;
+function comenzarApp() {
+    username = DarValorInput("Nombre")
+    password = DarValorInput("Contraseña")
+    user = CreateStudent(username,password)
+    Object.seal(user)
+    if (username.length < 4){
+        document.getElementById("Mensajemalo").innerText = "Tu nombre debe tener al menos 4 caracteres"
+    } else if (password.length < 8){
+        document.getElementById("Mensajemalo").innerText = "Tu contraseña debe tener al menos 8 caracteres"
+    } else {
+        document.getElementById("Mensajemalo").innerText = ""
+        seccion.classList.add("invisible")
+        document.getElementById("cursos").classList.add("hacking-courses")
+        document.getElementById("cursos").classList.remove("invisible")
+    }
+}
+sendbtn.addEventListener("click", comenzarApp)
